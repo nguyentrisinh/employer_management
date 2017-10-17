e5f466ab01796a56fcbdee29a851151667a5115c
Sinhnt <nguyentrisinh0810@gmail.com>
2017-10-17 20:34:01 +0700

    add reactjs to django project

--- employer_management/employer_management/urls.py
+++ employer_management/employer_management/urls.py
@@ -22,7 +22,7 @@ schema_view = get_swagger_view(title='Kawan Cicil APIs')
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('src.urls', namespace='src')),
-    url(r'^employer_site/', include('employer_site.urls', namespace='employer site')),
+    url(r'^', include('employer_site.urls', namespace='employer site')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^swagger/', schema_view)
 ]
